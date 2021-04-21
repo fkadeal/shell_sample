e134607077f2e057846a781f1d10a79e17f7f1c4
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-04-20 21:33:09 -0700

    Aq4

--- p3/geoQ.c
+++ p3/geoQ.c
@@ -8,3 +8,16 @@ Discuss o the major problems of urban centres of Ethiopia
 Explain brifly the contribution and challenges of tourism in ethiopia
 
 describe the contributions and challenges of transport sector to the ethiopian economy
+>>The city's current transportation system is marked by a poor access network, 
+  lack of adequate public transport services, continuous increases in transport fees, 
+  lack of smooth traffic flow and infrastructure for pedestrians, and a high rate of traffic accidents
+  
+>>public transport system and road construction underway in all corners of the city commuters face daily transport challenges and traffic congestion, 
+especially during rush hours, when on average one needs to wait for at least 15 to 25 minutes to get into a bus or taxi.
+  
+>> public transport services, continuous increases in transport fees, lack of smooth traffic flow and infrastructure for pedestrians, 
+and a high rate of traffic accidents.
+
+>> ecnomey
+>>Transport system helps to send raw materials, fuel and machineries to different industries at right time and runs the industry. 
+  It helps to raise the production of raw materials, fuels and machineries etc. by providing market to it. It widens the size of the market
