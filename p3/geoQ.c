064f813f32c22734c5eb0791f1ea68b79c8a7e3f
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-04-20 21:20:53 -0700

    q2,q3,q4

--- p3/geoQ.c
+++ p3/geoQ.c
@@ -1,3 +1,10 @@
 #include <fkadeal>
 the hig ad rugged relif nature of ethiopia is mostly accelerating the magnitude of soil erosion, 
 what are the mechanisms that are widely applied in the context of ethiopia to control soil erosion ?
+
+  
+Discuss o the major problems of urban centres of Ethiopia
+
+Explain brifly the contribution and challenges of tourism in ethiopia
+
+describe the contributions and challenges of transport sector to the ethiopian economy
