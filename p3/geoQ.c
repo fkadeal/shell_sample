edab27541e1435a813eb39b3d23c8585a4eaa734
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-04-20 20:52:30 -0700

    q1

---
+++ p3/geoQ.c
@@ -0,0 +1,3 @@
+#include <fkadeal>
+the hig ad rugged relif nature of ethiopia is mostly accelerating the magnitude of soil erosion, 
+what are the mechanisms that are widely applied in the context of ethiopia to control soil erosion ?
